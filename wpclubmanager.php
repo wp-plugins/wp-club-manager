b9ff0926de7deac6b199775c6ae7b7df83c742f9
ClubPress <ClubPress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-03 20:33:49 +0000

    Update to 1.2.19
    git-svn-id: https://plugins.svn.wordpress.org/wp-club-manager/trunk@1173934 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpclubmanager.php
+++ wpclubmanager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Club Manager
- * Version: 1.2.18
+ * Version: 1.2.19
  * Plugin URI: https://wpclubmanager.com
  * Description: A plugin to help you run a sports club website easily and quickly.
  * Author: Clubpress
@@ -31,7 +31,7 @@ final class WPClubManager {
 	/**
 	 * @var string
 	 */
-	public $version = '1.2.18';
+	public $version = '1.2.19';
 
 	/**
 	 * @var WPClubManager The single instance of the class
